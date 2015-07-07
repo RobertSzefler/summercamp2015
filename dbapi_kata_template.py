5bba3a0b27cde54db4232799fab17ef98b515055
Robert Szefler <robert.szefler@gmail.com>
2015-07-07 19:39:22 +0200

    missing file

--- dbapi_kata_template.py
+++ dbapi_kata_template.py
@@ -4,6 +4,6 @@ from MySQLdb import connect
 
 connection = connect(host='localhost', user='test', passwd='test', db='test')
 cursor = connection.cursor()
-cursor.execute('''SELECT id, name FROM persons''')
+cursor.execute('SELECT id, name FROM persons')
 for id, name in cursor.fetchall():
     print(id, name)
